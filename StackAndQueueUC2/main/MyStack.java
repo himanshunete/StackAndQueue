a27fd72649778197478c31fa9fddcaa2e2d3e1dd
himanshunete <himanshuneteh@gmail.com>
2021-01-27 11:47:15 +0530

    ADDED: functionality to peak and pop from stack

---
+++ StackAndQueueUC2/main/MyStack.java
@@ -0,0 +1,35 @@
+package javapractiseuc2;
+
+public class MyStack {
+	private final MyLinkedList myLinkedList ;
+	
+	public MyStack() {
+		this.myLinkedList = new MyLinkedList();
+	}
+	
+	public void push(INode myNode) {
+		myLinkedList.add(myNode);
+	}
+	
+	public void printStack() {
+		myLinkedList.printMyNodes();
+	}
+	
+	public INode peak() {
+		return myLinkedList.head ;
+		
+	}
+	
+	public INode pop() {
+		return myLinkedList.pop() ;
+		
+	}
+	
+	public INode isEmpty() {
+		myLinkedList.printMyNodes();
+		return null;
+	}
+	
+	
+	
+}
