17001e25dede7fdbb7a0bb055a9446dcb94a53a2
himanshunete <himanshuneteh@gmail.com>
2021-01-27 11:35:30 +0530

    ADDED: functionality to create a Queue

---
+++ Queue/main/MyQueue.java
@@ -0,0 +1,36 @@
+package javapractiseuc4;
+
+public class MyQueue {
+	private final MyLinkedList myLinkedList ;
+	
+	public MyQueue() {
+		this.myLinkedList = new MyLinkedList();
+	}
+	
+	public void enqueue(INode myNode) {
+		myLinkedList.append(myNode);
+	}
+	
+	public void printQueue() {
+		myLinkedList.printMyNodes();
+	}
+	
+	public INode peak() {
+		return myLinkedList.head ;
+		
+	}
+	
+	public INode dequeue() {
+		return myLinkedList.pop() ;
+		
+	}
+	
+	public INode isEmpty() {
+		myLinkedList.printMyNodes();
+		return null;
+	}
+	
+	
+	
+	
+}
